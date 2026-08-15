9158babb935d227341ca3480056b68f0b5737a25
huukhanh2612 <phanhuukhanh2612@gmail.com>
2026-08-16 01:07:22 +0700

    Update event.js

--- js/audio/event.js
+++ js/audio/event.js
@@ -1,16 +1,15 @@
 let loudState = false;
 
-const LOUD_THRESHOLD = 60;
-const RESET_THRESHOLD = 45;
+const LOUD_THRESHOLD = 50;
+const RESET_THRESHOLD = 30;
 
 export function detectSoundEvent(level) {
 
-    // Đang ở trạng thái bình thường
     if (!loudState && level >= LOUD_THRESHOLD) {
 
         loudState = true;
 
-        if (level >= 80) {
+        if (level >= 75) {
             return {
                 type: "VERY_LOUD_DETECTED",
                 level: Math.round(level),
@@ -25,14 +24,9 @@ export function detectSoundEvent(level) {
         };
     }
 
-
-    // Chỉ cho phép phát hiện event mới
-    // khi âm thanh đã trở lại đủ thấp
     if (loudState && level <= RESET_THRESHOLD) {
-
         loudState = false;
     }
-
 
     return null;
 }
