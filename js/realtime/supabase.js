f58405ebed76840d90be8824517e1480e5d13824
huukhanh2612 <phanhuukhanh2612@gmail.com>
2026-08-16 01:26:40 +0700

    Add Supabase client initialization in supabase.js

---
+++ js/realtime/supabase.js
@@ -0,0 +1,14 @@
+import { createClient } from
+    "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const SUPABASE_URL =
+    "https://zvddpvssfcfvsdjgjqrv.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_TI5upr-0Ie7mDHc41fTPTQ_CqGj9GF8";
+
+export const supabase =
+    createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
